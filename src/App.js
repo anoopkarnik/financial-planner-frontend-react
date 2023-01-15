27f782615fb63fc652dd6d2a2aecfacd628d90eb
anoopkarnik <danoopkarnik@gmail.com>
2023-01-15 19:25:15 +0530

    ec2 backend

--- src/App.js
+++ src/App.js
@@ -6,13 +6,13 @@ import Layout from './components/layout/Layout';
 import TransactionsPage from './components/pages/TransactionsPage';
 import AccountsPage from './components/pages/AccountsPage';
 import BudgetsPage from './components/pages/BudgetsPage'
-import React,{useState,useEffect} from 'react'
+import React,{useState} from 'react'
 import AdminPage from './components/pages/AdminPage';
 import FundsPage from './components/pages/FundsPage';
 
 function App() {
-	const backend_url = 'http://localhost:8082';
-    const [userId,setUserId] = useState(20);
+	const backend_url = 'http://65.0.12.84:8082';
+    const [userId] = useState(20);
 
   	return (
 		<BrowserRouter>
